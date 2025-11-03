5efabc6765d4e0f402f6e369e8527326231e95de
VarunRGB <144683358+VarunRGB@users.noreply.github.com>
2025-11-03 14:15:14 +0530

    Last minute changes

--- Fake_News_Det.py
+++ Fake_News_Det.py
@@ -6,20 +6,27 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
-tfvect = TfidfVectorizer(stop_words='english', max_df=0.7)
-loaded_model = pickle.load(open('model.pkl', 'rb'))
-dataframe = pd.read_csv('news.csv')
-x = dataframe['text']
-y = dataframe['label']
+tfvect = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
+loaded_model = pickle.load(open('model1.pkl', 'rb'))
+dataframe = pd.read_csv('cleaned_IFND.csv')
+x = dataframe['Statement']
+y = dataframe['Label']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
+import numpy as np
+
 def fake_news_det(news):
-    tfid_x_train = tfvect.fit_transform(x_train)
-    tfid_x_test = tfvect.transform(x_test)
     input_data = [news]
     vectorized_input_data = tfvect.transform(input_data)
     prediction = loaded_model.predict(vectorized_input_data)
-    return prediction
+
+    # Get confidence from decision function
+    decision_score = loaded_model.decision_function(vectorized_input_data)
+    
+    # Convert to pseudo-confidence between 0–100%
+    confidence = 1 / (1 + np.exp(-abs(decision_score[0]))) * 100  # sigmoid scaling
+    
+    return prediction, round(confidence, 2)
 
 @app.route('/')
 def home():
@@ -29,11 +36,11 @@ def home():
 def predict():
     if request.method == 'POST':
         message = request.form['message']
-        pred = fake_news_det(message)
-        print(pred)
-        return render_template('index.html', prediction=pred)
+        pred, confidence = fake_news_det(message)
+        print(pred, confidence)
+        return render_template('index.html', prediction=pred, confidence=confidence, message=message)
     else:
-        return render_template('index.html', prediction="Something went wrong")
+        return render_template('index.html', prediction="Something went wrong", confidence=None, message="")
 
 if __name__ == '__main__':
     app.run(debug=True)
